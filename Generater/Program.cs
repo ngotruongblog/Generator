9eb3b980cb71e04e85c718afe30513d64468a1db
NgoTuanTruong <tringochinh@gmail.com>
2021-09-01 20:28:53 +0700

    udpate

--- Generater/Program.cs
+++ Generater/Program.cs
@@ -14,8 +14,8 @@ namespace Generater
             var handler1 = Handler.GetHandlerCreate(_nameSpaceModels, _pathOutput, _nameProject);
             handler1.Process();
             // Replace file
-            //var _pathBuilder = @"D:\New folder\BookManager\src\BookManager.EntityFrameworkCore\EntityFrameworkCore\BookManagerDbContextModelCreatingExtensions.cs";
-            //var _nameModule = "LoyaltyService"; //Ten Microservice module
+            //var _pathBuilder = @"path of xxxDbContextModelCreatingExtensions.cs";
+            //var _nameModule = "TenService"; //Ten Microservice module
             //var handler2 = Handler.GetHandlerReplace(_pathBuilder, _nameModule, _nameSpaceModels);
             //handler2.Process();
         }
